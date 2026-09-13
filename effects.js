415cdd79b6a86e58e79e05fb020ee30653b01b6f
Saravanakumar-A100108 <saravanakumara100108@gmail.com>
2026-09-13 21:38:26 +0530

    Add subtle text entrance effects

--- effects.js
+++ effects.js
@@ -1,8 +1,14 @@
 const ticketLink = document.getElementById('ticketLink');
 const poster = document.querySelector('.poster');
 const foodNote = document.querySelector('.food-note');
+const textElements = document.querySelectorAll('.brand, .presents, .callig, .meta-row, .venue, .food-note, .highlights, .fewseats, .ticket-btn');
 
 ticketLink.href = 'https://chat.whatsapp.com/EBIiFRU9toR2VHtjAu5Ugw?s=sh&p=i&mlu=4';
+
+textElements.forEach((element, index) => {
+  element.style.animationDelay = `${index * 70}ms`;
+  element.classList.add('text-effect');
+});
 
 requestAnimationFrame(() => {
   poster.classList.add('is-ready');
